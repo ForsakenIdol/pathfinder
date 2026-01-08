2bf37a34fc8334b96514cee2d0b2a640fe4de61a
Kali <kali@example.com>
2026-01-08 13:07:20 +0800

    Wrap script logic in a main function

--- pathfinder.sh
+++ pathfinder.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
-IFS=: # Change the default character used to split a string to a colon ':'
-for PATHNAME in $(echo "$PATH" | sort | uniq); do
-    test -d "$PATHNAME" || continue # If $PATHNAME is not a directory, then continue to the next path name
-    find "$PATHNAME" -maxdepth 1 -type f -executable -print
-done
+print_executables() {
+    IFS=: # Change the default character used to split a string to a colon ':'
+    for PATHNAME in $(echo "$PATH" | sort | uniq); do
+        test -d "$PATHNAME" || continue # If $PATHNAME is not a directory, then continue to the next path name
+        find "$PATHNAME" -maxdepth 1 -type f -executable -print
+    done
+}
+
+main() {
+    print_executables
+}
+
+main
